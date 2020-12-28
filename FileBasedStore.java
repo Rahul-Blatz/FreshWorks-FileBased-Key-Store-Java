f1d7b0d77f89dc47f3f623434298aa6aa7ae51ca
Rahul Babu <rahulblatz002@gmail.com>
2020-12-28 16:38:21 +0530

    Add comments

--- FileBasedStore.java
+++ FileBasedStore.java
@@ -5,6 +5,7 @@ import org.json.simple.JSONObject;
 class Value {
 
     JSONObject value;
+    int timeOut;
 
     Value(int timeOut, JSONObject value) {
         this.timeOut = timeOut;
@@ -17,17 +18,19 @@ public class FileBasedStore {
     private static HashMap<String, Value> file;
 
     public static void createData(String key, JSONObject val) {
-        if (file == null) {
+	
+        if (file == null) {//checking if file is already created
             file = new HashMap<String, Value>();
         }
 
-        if (file.containsKey(key)) {
+	
+        if (file.containsKey(key)) { //checking if the file contains the key
             System.err.println("Error: The key " + key + " is already created");
         } else {
-            if (key.length() > 32) {
+            if (key.length() > 32) { //check for length not greater than 32
                 System.err.println("Length of the key is greater that 32");
             } else {
-                if ((file.size() < (1024 * 1024 * 1024)) && (val.size() < 16 * 1024 * 1024) && key.length() <= 32) {
+                if ((file.size() < (1024 * 1024 * 1024)) && (val.size() < 16 * 1024 * 1024) && key.length() <= 32) { //checking if data store file exceeded 1GB or the json file is larger than 16KB
                     int timeOut = 0;
                     Value valWithTimeout = new Value(timeOut, val);
 
@@ -39,14 +42,20 @@ public class FileBasedStore {
         }
     }
 
+    //method for optional timeout parameter
     public static void createData(String key, JSONObject val, int timeOut) {
-        if (file.containsKey(key)) {
+        
+	if (file == null) {//checking if file is already created
+            file = new HashMap<String, Value>();
+        }
+
+        if (file.containsKey(key)) { //check for length not greater than 32
             System.err.println("Error: This key " + key + " is already created");
         } else {
 	    if (key.length() > 32) {
                 System.err.println("Length of the key is greater that 32");
             } else {
-                if ((file.size() < (1024 * 1024 * 1024)) && (val.size() < 16 * 1024 * 1024) && key.length() <= 32) {
+                if ((file.size() < (1024 * 1024 * 1024)) && (val.size() < 16 * 1024 * 1024) && key.length() <= 32) { //checking if data store file exceeded 1GB or the json file is larger than 16KB
                     timeOut = (int) (Instant.now().getEpochSecond() + timeOut);
                     Value valWithTimeout = new Value(timeOut, val);
                     file.put(key, valWithTimeout);
@@ -58,6 +67,10 @@ public class FileBasedStore {
     }
 
     public static void readData(String key) {
+	if (file == null) {
+            System.err.println("Error: No data store is initialized");
+        }
+
         if (file.containsKey(key)) {
             Value value = file.get(key);
             if ((value.timeOut > (int) (Instant.now().getEpochSecond())) || (value.timeOut == 0)) {
@@ -72,6 +85,10 @@ public class FileBasedStore {
     }
 
     public static void deleteData(String key) {
+	if (file == null) {
+            System.err.println("Error: No data store is initialized");
+        }
+
         if (file.containsKey(key)) {
             Value value = file.get(key);
             if ((value.timeOut < Instant.now().getEpochSecond())) {
