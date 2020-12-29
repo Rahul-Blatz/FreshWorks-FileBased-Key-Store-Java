56e320c781266d4832b212a4ea182e7130cc37b2
Rahul Babu <rahulblatz002@gmail.com>
2020-12-29 17:25:20 +0530

    Change in Java file

--- FileBasedStore.java
+++ FileBasedStore.java
@@ -1,3 +1,4 @@
+
 import java.time.Instant;
 import java.util.HashMap;
 import org.json.simple.JSONObject;
@@ -18,12 +19,11 @@ public class FileBasedStore {
     private static HashMap<String, Value> file;
 
     public static void createData(String key, JSONObject val) {
-	
+
         if (file == null) {//checking if file is already created
             file = new HashMap<String, Value>();
         }
 
-	
         if (file.containsKey(key)) { //checking if the file contains the key
             System.err.println("Error: The key " + key + " is already created");
         } else {
@@ -44,15 +44,15 @@ public class FileBasedStore {
 
     //method for optional timeout parameter
     public static void createData(String key, JSONObject val, int timeOut) {
-        
-	if (file == null) {//checking if file is already created
+
+        if (file == null) {//checking if file is already created
             file = new HashMap<String, Value>();
         }
 
         if (file.containsKey(key)) { //check for length not greater than 32
             System.err.println("Error: This key " + key + " is already created");
         } else {
-	    if (key.length() > 32) {
+            if (key.length() > 32) {
                 System.err.println("Length of the key is greater that 32");
             } else {
                 if ((file.size() < (1024 * 1024 * 1024)) && (val.size() < 16 * 1024 * 1024) && key.length() <= 32) { //checking if data store file exceeded 1GB or the json file is larger than 16KB
@@ -67,42 +67,38 @@ public class FileBasedStore {
     }
 
     public static void readData(String key) {
-	if (file == null) {
+        if (file == null) {
             System.err.println("Error: No data store is initialized");
-        }
-
-        else{
-	    if (file.containsKey(key)) {
-            Value value = file.get(key);
-            if ((value.timeOut > (int) (Instant.now().getEpochSecond())) || (value.timeOut == 0)) {
-//               System.out.println(value.timeOut+" : "+Instant.now().getEpochSecond());
-                System.out.println(key + " : " + value.value);
-            } else {
-                System.err.println("Error: Time has expired for key: " + key);
-            }
         } else {
-            System.err.println("Error : The key is not found in the DataStore");
+            if (file.containsKey(key)) {
+                Value value = file.get(key);
+                if ((value.timeOut > (int) (Instant.now().getEpochSecond())) || (value.timeOut == 0)) {
+//               System.out.println(value.timeOut+" : "+Instant.now().getEpochSecond());
+                    System.out.println(key + " : " + value.value);
+                } else {
+                    System.err.println("Error: Time has expired for key: " + key);
+                }
+            } else {
+                System.err.println("Error : The key is not found in the DataStore");
+            }
         }
-	}
     }
 
     public static void deleteData(String key) {
-	if (file == null) {
+        if (file == null) {
             System.err.println("Error: No data store is initialized");
-        }
-
-        else{
-	   if (file.containsKey(key)) {
-            Value value = file.get(key);
-            if ((value.timeOut < Instant.now().getEpochSecond())) {
-                file.remove(key);
-                System.out.println("The data with the key "+key+" has been removed");
-            } else {
-                System.err.println("Error: Time has expired for key: " + key);
-            }
         } else {
-            System.err.println("Error: Key does not Exist");
+            if (file.containsKey(key)) {
+                Value value = file.get(key);
+                if ((value.timeOut < Instant.now().getEpochSecond())) {
+                    file.remove(key);
+                    System.out.println("The data with the key " + key + " has been removed");
+                } else {
+                    System.err.println("Error: Time has expired for key: " + key);
+                }
+            } else {
+                System.err.println("Error: Key does not Exist");
+            }
         }
-	}
     }
 }
